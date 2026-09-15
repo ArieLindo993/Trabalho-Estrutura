b61d2c7ff316211843564ccad758b5e99a2fb282
Ariel <arieltorrente4@gmail.com>
2026-09-15 17:24:38 -0300

    Commit inicial - trabalho estrutura

---
+++ src/test/java/com/example/Trabalho_Estrutura/TrabalhoEstruturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trabalho_Estrutura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoEstruturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
